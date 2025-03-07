ad60ee65c338527d3efe421af1068235ce8e49b4
Damian-Oswald <damian.oswald@protonmail.com>
2025-03-07 11:35:27 +0100

    Hotfix: Revert to absolute path

--- docs/assets/js/details.js
+++ docs/assets/js/details.js
@@ -2,7 +2,7 @@
 
 // Configuration
 const branch = "main";
-const baseDataUrl = `https://raw.githubusercontent.com/blw-ofag-ufag/data-catalog/refs/heads/${branch}/docs/data/datasets/`;
+const baseDataUrl = `https://raw.githubusercontent.com/blw-ofag-ufag/data-catalog/refs/heads/${branch}/data/datasets/`;
 
 // List of enumerated fields to highlight
 const enumeratedFields = [
