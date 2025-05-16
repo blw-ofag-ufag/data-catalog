cb08ca39fcfba43a3817d52909745edee4977429
bar9 <rolandbrand11@gmail.com>
2025-05-16 11:06:00 +0200

    config file format for publishers (not used in API service yet)

---
+++ src/app/services/api/publisher.service.ts
@@ -0,0 +1,24 @@
+import {Injectable} from '@angular/core';
+import * as publisherData from '../../codegen/publishers.json';
+import {Publisher} from '../../models/publisher.model';
+
+@Injectable({
+	providedIn: 'root'
+})
+export class PublisherService {
+	private readonly publishers: Publisher[];
+
+	constructor() {
+		this.publishers = publisherData as Publisher[];
+		for (const publisher of this.publishers) {
+			publisher.getProcessedUrl = () =>
+				`https://raw.githubusercontent.com/${publisher.githubRepo}/refs/heads/${publisher.branch}/data/processed/datasets.json`;
+			publisher.getDetailUrl = (id) =>
+				`https://raw.githubusercontent.com/${publisher.githubRepo}/refs/heads/${publisher.branch}/data/raw/datasets/${id}.json}`;
+		}
+	}
+
+	getPublishers(): Publisher[] {
+		return this.publishers;
+	}
+}
